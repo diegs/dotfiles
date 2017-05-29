00c1a7e1f96cbb5704ff4d46762120df375ed748
Diego Pontoriero <diego.pontoriero@coreos.com>
2017-05-29 13:41:18 -0700

    Update a bunch of configs.

--- .nixos/configuration.nix
+++ .nixos/configuration.nix
@@ -6,31 +6,52 @@
     efi.canTouchEfiVariables = true;
     systemd-boot.enable = true;
   };
+  environment.interactiveShellInit = ". ${pkgs.gnome3.vte}/etc/profile.d/vte.sh";
   environment.systemPackages = with pkgs; [
+    aws
     dmenu
     i3blocks
     i3-gaps
     i3lock
+    i3status
     jsoncpp
     git
     gnupg
     gnumake
     go
     google-chrome
+    google-cloud-sdk
+    gotools
+    htop
+    jq
+    kubernetes
     lemonbar
     libu2f-host
     lxappearance
+    nodejs
+    pciutils
+    pinentry
     polybar
+    python
+    ripgrep
     rofi
+    screenfetch
+    sxhkd
     slack
     spotify
-    steam
     termite
     tree
     vanilla-dmz
     vimHugeX
+    xclip
+    xorg.xbacklight
     xss-lock
+    unzip
+    usbutils
+    vagrant
+    wpa_supplicant_gui
     yabar
+    yarn
     yubikey-neo-manager
   ];
   fonts = {
@@ -71,12 +92,27 @@
   services = {
     journald.extraConfig = "SystemMaxUse=50M";
     pcscd.enable = true;
+    thermald.enable = true;
     udev.packages = with pkgs; [ libu2f-host ];
     xserver = {
+      displayManager.slim = {
+        defaultUser = "diegs";
+        enable = true;
+        theme = pkgs.fetchurl {
+          url = "https://github.com/edwtjo/nixos-black-theme/archive/v1.0.tar.gz";
+          sha256 = "13bm7k3p6k7yq47nba08bn48cfv536k4ipnwwp1q1l2ydlp85r9d";
+        };
+      };
       enable = true;
       layout = "us";
-      displayManager.lightdm.enable = true;
-      windowManager.bspwm.enable = true;
+      windowManager = {
+        bspwm.enable = true;
+        default = "i3";
+        i3 = {
+          enable = true;
+          package = pkgs.i3-gaps;
+        };
+      };
     };
   };
   system = {
@@ -91,7 +127,7 @@
   };
   users.extraUsers.diegs = {
     isNormalUser = true;
-    extraGroups = [ "docker" "networkmanager" "rkt" "vboxusers" "wheel" ];
+    extraGroups = [ "docker" "rkt" "vboxusers" "wheel" ];
     password = "changeme";
     uid = 1000;
   };
