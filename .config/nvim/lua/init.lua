15ac0a4b40f0801acfd256dccbb3aeb44bf9ea7c
Diego Pontoriero <74719+diegs@users.noreply.github.com>
2021-07-14 20:03:28 -0700

    remap dammit

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -2,7 +2,7 @@ local g = vim.g
 local o = vim.opt
 local cmd = vim.cmd
 local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
+  local options = {}
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
