8491672dc10fb26e9b5cc9db1ceb2cfafbc1627d
Diego Pontoriero <74719+diegs@users.noreply.github.com>
2023-03-13 13:37:03 -0700

    flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "diegs home-manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {nixpkgs, home-manager, ...}:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+      defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+      defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+
+      homeConfigurations.diegs = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ 
+          ./home.nix 
+        ];
+      };
+    };
+}
