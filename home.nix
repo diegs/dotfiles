e1844ea5d5275dc3eabf29e6a347b2bd88d63e41
Diego Pontoriero <74719+diegs@users.noreply.github.com>
2023-03-14 14:31:29 -0700

    latest jdk

--- home.nix
+++ home.nix
@@ -221,13 +221,13 @@
           display-messages = false;
         };
         mouse = false;
-        whitespace = {
-          render = {
-            space = "all";
-            tab = "all";
-            newline = "none";
-          };
-        };
+        # whitespace = {
+        #   render = {
+        #     space = "all";
+        #     tab = "all";
+        #     newline = "none";
+        #   };
+        # };
       };
       keys.normal = {
         ";" = "repeat_last_motion";
@@ -270,7 +270,7 @@
 
   programs.java = {
     enable = true;
-    package = pkgs.jdk11_headless; 
+    package = pkgs.jdk_headless; 
   };
 
   programs.jq = {
