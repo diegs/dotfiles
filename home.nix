ab8b1bc7458df84ce07c43c61df006905226074c
Diego Pontoriero <74719+diegs@users.noreply.github.com>
2023-07-18 11:08:50 -0700

    tweak-er

--- home.nix
+++ home.nix
@@ -105,19 +105,16 @@ in {
       # '')
     ];
 
-    # Home Manager is pretty good at managing dotfiles. The primary way to manage
-    # plain files is through 'home.file'.
     file = {
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
       # ".screenrc".source = dotfiles/screenrc;
-  
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
+
+      ".config/1Password/ssh/agent.toml".text = ''
+        [[ssh-keys]]
+        vault = "Private"
+
+        [[ssh-keys]]
+        vault = "DevOps"
+      '';
     };
   
     sessionVariables = {
