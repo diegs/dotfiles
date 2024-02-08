6d63c8c82fa209b50f0266abe1b01b386f338b97
Diego Pontoriero <74719+diegs@users.noreply.github.com>
2024-02-07 20:26:14 -0500

    update flake.lock

--- home.nix
+++ home.nix
@@ -47,11 +47,11 @@ in {
       pkgs.go-migrate
       pkgs.graphite-cli
       # (pkgs.nodePackages.graphite-cli.override (_: {
-      #   version = "1.1.2";
+      #   version = "1.1.4";
       #   src = pkgs.fetchurl {
-      #     url = "https://registry.npmjs.org/@withgraphite/graphite-cli/-/graphite-cli-0.22.15.tgz";
-      #     sha512 = "sha512-LshB8BhJrlLUhFG5H4gvpVca5R8p7UM8CSKVrIbYiRQ5y+9ASZ2st1zhITl0FwAQ6o4ZDN6vFK/1CCXy/OKPmw==";
-      #     # sha512 = lib.fakeSha512;
+      #     url = "https://registry.npmjs.org/@withgraphite/graphite-cli/-/graphite-cli-1.1.4.tgz";
+      #     # sha512 = "sha512-LshB8BhJrlLUhFG5H4gvpVca5R8p7UM8CSKVrIbYiRQ5y+9ASZ2st1zhITl0FwAQ6o4ZDN6vFK/1CCXy/OKPmw==";
+      #     sha512 = lib.fakeSha512;
       #   };
       # }))
       pkgs.python3Packages.grip
